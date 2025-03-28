1b6157182a8d45f54570880b3daf1d420d211b2a
MauGomezp <changeme@example.com>
2025-03-28 16:16:31 +0000

    Agrego de Buenas

--- Calculadora.py
+++ Calculadora.py
@@ -5,3 +5,4 @@ Num2 = int(input("Ingrese el segundo numero: "))
 
 print(f"La suma de los dos valores es {Num1 + Num2}")
 print(f"La resta de los dos valores es {Num1 - Num2}")
+print("Buenas a todos gente")
